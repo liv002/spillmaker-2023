3aa99bd2de29c9bb67082a8a4c08e8ac37e21197
liv002 <43001067+liv002@users.noreply.github.com>
2023-06-01 16:34:56 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -4,44 +4,18 @@
 * Read more at https://arcade.makecode.com/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 
 /**
  * Custom blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace custom {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
-
+namespace test {
     /**
      * TODO: describe your function here
      * @param asteroid some Sprite that will do this behavior
      */
     //% block
-    export function logic(asteroid: Sprite) {
+    export function logic(asteroid: Sprite): void {
         asteroid.setVelocity(0, 75)
         asteroid.setPosition(randint(3, 150), 0)
     }

7fa5f9653893faf9907e21c2fa886331dddabc61
liv002 <43001067+liv002@users.noreply.github.com>
2023-06-03 20:10:23 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,17 +1,9 @@
-
-/**
-* Use this file to define custom functions and blocks.
-* Read more at https://arcade.makecode.com/blocks/custom
-*/
-
-
 /**
  * Custom blocks
  */
 //% weight=100 color=#c381bc icon="\e076"
 namespace enemy {
     /**
-     * TODO: describe your function here
      * @param asteroid some Sprite that will do this behavior
      */
     //% block

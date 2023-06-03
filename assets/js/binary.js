e85993e53ca236ef2cd6186439443ff3a99561e6
liv002 <43001067+liv002@users.noreply.github.com>
2023-06-03 20:49:00 +0200

    2.2.0

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.30/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"2.1.6","target":"arcade","targetVersion":"1.12.30","repo":"liv002/spillmaker-2023"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.30/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"2.2.0","target":"arcade","targetVersion":"1.12.30","repo":"liv002/spillmaker-2023"}
 // total=1247292 new=5.85% cached=88.48% other=5.67%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P61150(s) {
+function _main___P62000(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,13 +831,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P61150.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P61150.continuations = [  ]
+_main___P62000.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P62000.continuations = [  ]
 
-function _main___P61150_mk(s) {
+function _main___P62000_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P61150, depth: s.depth + 1,
+        parent: s, fn: _main___P62000, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42295,5 +42295,5 @@ const sprites_BaseSpriteSayRenderer__C42696_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P61150
+return _main___P62000
 })

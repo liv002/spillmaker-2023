2c4a152ff79d75d639887d80118ad05b62f7513c
liv002 <43001067+liv002@users.noreply.github.com>
2023-06-03 20:26:19 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,7 +1,7 @@
 /**
  * Custom blocks
  */
-//% weight=100 color=#c381bc icon="\f1ec"
+//% weight=100 color=#c381bc icon="\uf753"
 namespace enemy {
     /**
      * @param asteroid some Sprite that will do this behavior

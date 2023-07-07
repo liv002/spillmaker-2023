4eef2198c8392ce6fbfc7b085055fe9462137fe8
liv002 <43001067+liv002@users.noreply.github.com>
2023-07-07 14:19:15 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,11 +1,12 @@
 /**
  * Custom blocks
+ * 
  */
 //% weight=100 color=#c381bc icon="\uf753"
 namespace enemy {
     /**
      * @param asteroid some Sprite that will do this behavior, eg: asteroid
-     */
+     */ 
     //% block
     export function movement(asteroid: Sprite): void {
         asteroid.setVelocity(0, 75)
